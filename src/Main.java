3e02e924b48a49eaef8737dfa70029d1ac91b69f
osoki <osokinkonstantin@mail.ru>
2024-08-04 21:15:36 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            SecurityUtils.check("Player1", "qaz123456/", "qaz123456");
+            SecurityUtils.check("Player1", "qaz123456", "qaz123456");
         } catch (WrongPasswordException e) {
             System.out.println("Error!");
         }
